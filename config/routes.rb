b2b84242d876dcf71f1df9d6e6eca5c493f42b0f
Lara Mills <lkm347@gmail.com>
2020-08-25 15:31:36 +0100

    routes file started

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :philosophers do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  resources :philosophers do
+    resources :bookings, only: [ :new, :create, :destroy ]
 end
