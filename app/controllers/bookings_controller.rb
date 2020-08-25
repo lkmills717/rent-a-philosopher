658f4097f6cf3826bc9e6e281b70dd13dad7d9e6
Lara Mills <lkm347@gmail.com>
2020-08-25 10:41:32 +0100

    bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+    before_action :set_booking, only: [:new, :create]
+
+    def new
+        @booking = Booking.new
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+        @booking.save
+        if @booking.save
+            redirect_to philosopher_path(@philosopher)
+          else
+            render :new
+        end
+    end
+
+    private
+    def booking_params
+        params.require(:booking).permit(:user_id, :philosopher_id
+    end
+
+    def set_booking
+        @booking = Booking.find(params[:id])
+    end
 end
