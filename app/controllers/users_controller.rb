a15208a726f31b5580be600c6622dee8f994fddd
richard barrett <richardbarrett1981@hotmail.com>
2020-08-27 15:45:13 +0100

    Profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+	def dashboard
+		@past_bookings = current_user.bookings.where("date < ?", Date.current)
+		@future_bookings = current_user.bookings.where("date >= ?", Date.current)
+	end
 end
