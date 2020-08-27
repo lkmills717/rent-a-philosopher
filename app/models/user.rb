cf50e87249d718aec007ceb5ca493621ee7417be
Gabriel Neves <gabriel.bneves@gmail.com>
2020-08-27 12:36:00 +0100

    trying to fix show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    has_many :bookings
-    has_many :philosophers, through: :bookings
-    user = ["student", "agent"]
-    # validates :role, inclusion: { in %w(student agent) }
+  has_many :bookings
+  has_many :philosophers, through: :bookings
+  # user = ["student", "agent"]
+  # validates :role, inclusion: { in %w(student agent) }
 end
